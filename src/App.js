a7a43e6988c061bf206b8c06431fdc9e92c1efcc
Jever Osmanova <amidala274@gmail.com>
2020-10-06 21:11:09 +0300

    Создание простой маршрутизации

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, {Component, Suspense, lazy } from 'react';
+import NavbarAside from './navigation/Navbar';
+import './App.css';
+const MainPage = lazy(() => import('./pages/mainPage/pageMain'));
+const TestPage = lazy(() => import('./pages/testPage/pageTest'));
 
-
-function App() {
-  return;
+class App extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <aside className="app-aside">
+          <NavbarAside />
+        </aside>
+          <Router>
+              <Suspense fallback={<div>Loading...</div>}>
+                  <Switch>
+                      <Route exact path="/" component={ (()=>(<MainPage />)) } />
+                      <Route path="/test" component={(()=>(<TestPage />))} />
+                  </Switch>
+              </Suspense>
+          </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/testPage/pageTest.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TestPage extends Component {
+    render(){
+        return (
+            <main className="app-main">
+                <section id="testPage" className="section">
+                    <h1>Тестовая страница</h1>
+                </section>
+            </main>
+        );
+    }
+}
+
+export default TestPage;
