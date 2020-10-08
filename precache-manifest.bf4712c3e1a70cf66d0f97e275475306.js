fea16f94eb4b307dbeb793e088b80aada7c15ac8
Jever Osmanova <amidala274@gmail.com>
2020-10-08 21:30:30 +0300

    Updates

---
+++ precache-manifest.bf4712c3e1a70cf66d0f97e275475306.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2300115198f3ab3c2b778d535dbb96f",
+    "url": "/penknife/index.html"
+  },
+  {
+    "revision": "f2a67f3a80eec7a683a6",
+    "url": "/penknife/static/css/main.1d4ab72c.chunk.css"
+  },
+  {
+    "revision": "d3638a8933076a01bcec",
+    "url": "/penknife/static/js/2.2e90bf5c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/penknife/static/js/2.2e90bf5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1ccb57178d2fa3abdff",
+    "url": "/penknife/static/js/3.1f9009b6.chunk.js"
+  },
+  {
+    "revision": "606f8f5b8220a773359f",
+    "url": "/penknife/static/js/4.27e08045.chunk.js"
+  },
+  {
+    "revision": "f2a67f3a80eec7a683a6",
+    "url": "/penknife/static/js/main.e8507ef7.chunk.js"
+  },
+  {
+    "revision": "5734d81a84e7528d0123",
+    "url": "/penknife/static/js/runtime-main.e75e9726.js"
+  }
+]);
