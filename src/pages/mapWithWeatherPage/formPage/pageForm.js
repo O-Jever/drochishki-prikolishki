b59fb790ac9c2b5637b690f74a0452c955e6caf1
Jever Osmanova <amidala274@gmail.com>
2020-11-18 00:42:04 +0300

    feat(FormComponent): use material ui and yup

--- src/pages/mapWithWeatherPage/formPage/pageForm.js
+++ src/pages/mapWithWeatherPage/formPage/pageForm.js
@@ -1,36 +1,58 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import './pageForm.css';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+import { Button, TextField } from '@material-ui/core';
 
-const FormComponent = () => (
+const validationSchema = yup.object({
+    startPoint: yup
+      .string('Введите адрес')
+      .required('Поле обязательно для заполнения'),
+    endPoint: yup
+      .string('Введите адрес')
+      .required('Поле обязательно для заполнения'),
+});
+
+const FormComponent = () => {
+    const formik = useFormik({
+        initialValues: {
+            startPoint: 'Симферополь',
+            endPoint: 'Ялта',
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            console.log(JSON.stringify(values, null, 2));
+        },
+    });    
+
+    return (
     <div>
-        <Formik
-        initialValues={{ startPoint: 'Симферополь', endPoint: 'Ялта' }}
-        validate={values => {
-            const errors = {};
-            for(const value of ['startPoint', 'endPoint']) {
-                if (!values[value]) {
-                    errors[value] = 'Required';
-                }
-            }
-            return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-            setSubmitting(false);
-        }}
-        >
-        {({ isSubmitting }) => (
-            <Form>
-                <Field type="text" name="startPoint" />
-                <ErrorMessage name="startPoint" component="div" />
-                <Field type="text" name="endPoint" />
-                <ErrorMessage name="endPoint" component="div" />
-                <button type="submit" disabled={isSubmitting}>
-                    Предсказать
-                </button>
-            </Form>
-        )}
-        </Formik>
+        <form onSubmit={formik.handleSubmit} className="form">
+            <TextField
+                type="text"
+                name="startPoint"
+                className="form__input"
+                label="Пункт отправки"
+                value={formik.values.startPoint}
+                onChange={formik.handleChange}
+                error={formik.touched.startPoint && Boolean(formik.errors.startPoint)}
+                helperText={formik.touched.startPoint && formik.errors.startPoint}
+            />
+            <TextField
+                type="text"
+                name="endPoint"
+                className="form__input"
+                label="Пункт прибытия"
+                value={formik.values.endPoint}
+                onChange={formik.handleChange}
+                error={formik.touched.endPoint && Boolean(formik.errors.endPoint)}
+                helperText={formik.touched.endPoint && formik.errors.endPoint}
+            />
+            <Button variant="contained" color="primary" type="submit">
+                Предсказать
+            </Button>
+        </form>
     </div>
-);
+)};
 
 export default FormComponent;
